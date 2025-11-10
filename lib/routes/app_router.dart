7947541caa70e1f19834ca0111d3a936d4c303b2
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-11-10 18:40:27 +0300

    Create basic history screen and add route

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooply/features/home/presentation/screens/history_screen.dart';
 import 'package:hooply/features/home/presentation/screens/home_screen.dart';
 import 'package:hooply/features/home/presentation/screens/team_list_screen.dart';
 import 'package:hooply/routes/route_names.dart';
@@ -19,6 +20,12 @@ final routerProvider = Provider<GoRouter>((ref) {
         name: RouteNames.teamList,
         pageBuilder: (context, state) =>
             NoTransitionPage(child: const TeamListScreen()),
+      ),
+      GoRoute(
+        path: RouteNames.history,
+        name: RouteNames.history,
+        pageBuilder: (context, state) =>
+            NoTransitionPage(child: const HistoryScreen()),
       ),
     ],
   );
